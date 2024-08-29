7be46529f504ccb1ac22e9c5b5556471373ac201
Martin Rioja <cr.martin.24680@gmail.com>
2024-08-29 16:52:21 -0500

    Mejora proyecto base v2

--- src/main/java/pe/edu/upc/project_security_g06/controllers/UserControllers.java
+++ src/main/java/pe/edu/upc/project_security_g06/controllers/UserControllers.java
@@ -1,4 +1,10 @@
 package pe.edu.upc.project_security_g06.controllers;
 
+import jakarta.persistence.Column;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class UserControllers {
+
 }
--- src/main/java/pe/edu/upc/project_security_g06/servicesinterfaces/IdUserService.java
+++ src/main/java/pe/edu/upc/project_security_g06/servicesinterfaces/IdUserService.java
@@ -1,4 +1,16 @@
 package pe.edu.upc.project_security_g06.servicesinterfaces;
 
+import org.springframework.data.repository.query.Param;
+import pe.edu.upc.project_security_g06.dtos.UserDTO;
+import pe.edu.upc.project_security_g06.entities.User;
+
+import java.util.List;
+
 public interface IdUserService {
+    public List<User> list();
+    public void insert(User user);
+    public User listId(int id);
+    public void delete(int id);
+    public void update(User user);
+    public List<User> buscar (String username);
 }
