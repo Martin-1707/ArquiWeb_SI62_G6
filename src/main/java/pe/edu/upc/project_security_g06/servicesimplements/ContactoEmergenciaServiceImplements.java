c087594e69bf84c47b462769bd53e47df5201cc2
JulkPbl <cr.martin.24680@gmail.com>
2024-09-17 22:26:17 -0500

    Spring 1: Contacto_Emergencia

---
+++ src/main/java/pe/edu/upc/project_security_g06/servicesimplements/ContactoEmergenciaServiceImplements.java
@@ -0,0 +1,24 @@
+package pe.edu.upc.project_security_g06.servicesimplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.project_security_g06.entities.Contacto_Emergencia;
+import pe.edu.upc.project_security_g06.repositories.IContacEmergenciaRepository;
+import pe.edu.upc.project_security_g06.servicesinterfaces.IContacEmergenciaService;
+
+import java.util.List;
+
+@Service
+public class ContactoEmergenciaServiceImplements implements IContacEmergenciaService {
+    @Autowired
+    private IContacEmergenciaRepository ceR;
+    @Override
+    public void insert(Contacto_Emergencia contactoEmergencia) {
+        ceR.save(contactoEmergencia);
+    }
+
+    @Override
+    public List<Contacto_Emergencia> list() {
+        return ceR.findAll();
+    }
+}
